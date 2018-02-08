39e176575cc1be7a250d6409dec6ad49651eb7b6
aso4107 <andras.somagaal@springernature.com>
2018-02-08 17:14:32 +0000

    react experimenting

--- src/main/resources/static/flight_diagram.js
+++ src/main/resources/static/flight_diagram.js
@@ -1,11 +1,11 @@
 var DropdownFill = React.createClass({
 
   getIatas: function () {
-    var self = this;
     $.ajax({
       url: "http://localhost:8080/iatas"
     }).done(function(data) {
-        self.setState({iatas: data});
+    console.log("ajax call complete, got=" + data)
+        this.state = {iatas: data};
       });
   },
 
@@ -17,28 +17,31 @@ var DropdownFill = React.createClass({
     this.getIatas();
   },
 
-  render() {
-  var rows = [];
-      this.props.iatas.forEach(function(iata) {
-        rows.push(<DropdownMenuElement iata={iata}/>);
-      });
-      return ( <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">{rows}</div>);
-    return ( <DropdownMenu iatas={this.state.iatas}/> );
+  render: function() {
+//  var rows = [];
+  console.log(this.state)
+//      this.state.iatas.forEach(function(iata) {
+//        rows.push(<DropdownMenuElement iata={iata}/>);
+//      });
+//      console.log(rows)
+//  return rows;
+    return ( <DropdownMenuElement iata={this.state.iatas}/> );
   }
 });
 
-var DropdownMenu = React.createClass({
-render: function() {
-    var rows = [];
-    this.props.iatas.forEach(function(iata) {
-      rows.push(<DropdownMenuElement iata={iata}/>);
-    });
-    return ( <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">{rows}</div>);
-  }
-});
+//var DropdownMenu = React.createClass({
+//render: function() {
+//    var rows = [];
+//    this.props.iatas.forEach(function(iata) {
+//      rows.push(<DropdownMenuElement iata={iata}/>);
+//    });
+//    return ( <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">{rows}</div>);
+//  }
+//});
 
 var DropdownMenuElement = React.createClass({
   render: function() {
+  console.log("redering element = " + this.props.iata)
     return (<a class="dropdown-item">{this.props.iata}</a>);
   }
 });

f640e49747c99ba250438d18b09e515f302a131e
andrasgaal <andras.somagaal@springernature.com>
2018-02-13 22:44:20 +0000

    Station and date selection with react

--- src/main/resources/static/js/flight_diagram.js
+++ src/main/resources/static/js/flight_diagram.js
@@ -1,56 +1,128 @@
-var DropdownFill = React.createClass({
+class PriceDiagram extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            iatas: []
+        }
+    }
 
-    getIatas: function () {
-        console.log("get iatas called");
-        var self = this;
+    componentDidMount(){
+        this.getIatas();
+    }
+
+    getIatas(){
+        let self = this;
         $.ajax({
             url: "http://localhost:8080/iatas"
         }).done(function (data) {
-            console.log("ajax call complete, got=" + data);
             self.setState({iatas: data});
         });
-    },
+    }
 
-    getInitialState: function () {
-        console.log("component mounted");
-        return {iatas: []};
-    },
+    render(){
+        return (
+            <div>
+                <Dropdown iatas={this.state.iatas} station={"departure"} defaultIata={"BUD"} labelText={"From"}/>
+                <div className="separator"/>
+                <Dropdown iatas={this.state.iatas} station={"arrival"} defaultIata={"LTN"} labelText={"To"}/>
+                <div className="separator"/>
+                <DatePicker/>
+                <div className="separator"/>
+                <UpdateButton/>
+            </div>
+        );
+    }
+}
 
-    componentDidMount: function () {
-        console.log("get initial state");
-        this.getIatas();
-    },
+class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {chosenIata : this.props.defaultIata}
+    }
 
-    render: function () {
-        console.log("dropdown state=" + this.state);
+    handleClick(iata) {
+        this.setState({chosenIata: iata})
+    }
+
+    render(){
         let station = this.props.station;
-        console.log("dropdown station  props=" + station);
-        var rows = [];
-        this.state.iatas.forEach(function (iata) {
-            rows.push(<DropdownMenuElement iata={iata} station={station}/>);
+
+        let self = this;
+        let dropdownMenuElements = [];
+        this.props.iatas.forEach(function (iata) {
+            dropdownMenuElements.push(<DropdownMenuElement key={iata} iata={iata} onClick={() => self.handleClick(iata)}/>);
         });
-        return (<div>{rows}</div>);
-    }
-});
 
-var DropdownMenuElement = React.createClass({
-    handleClick() {
-        let iata = this.props.iata;
-        console.log(iata);
-        console.log(this.props.station);
-        $("#" + this.props.station + "-station").val(iata);
-    },
-
-    render: function () {
-        console.log("rendering element = " + this.props.iata);
-        return (<li className="dropdown-item" onClick={this.handleClick}>{this.props.iata}</li>);
+        return (
+            <div>
+                <label htmlFor={station +"-station-dropdown-button"}>{this.props.labelText}</label>
+                <div className="dropdown">
+                    <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" id={station +"-station-dropdown-button"}>
+                        {this.state.chosenIata}
+                    </button>
+                    <ul id={station +"-station-dropdown"} className="dropdown-menu pointer">
+                        {dropdownMenuElements}
+                    </ul>
+                </div>
+            </div>
+        )
     }
-});
+}
+
+function DropdownMenuElement(props){
+    return (<li className="dropdown-item" onClick={props.onClick}>{props.iata}</li>);
+}
+
+class DatePicker extends React.Component {
+    constructor() {
+        super();
+    }
+
+    componentDidMount(){
+        document.getElementById("flight-date").valueAsDate = new Date()
+    }
+
+    render() {
+        return (
+            <div>
+                <label htmlFor="flight-date">Flight date</label>
+                <div>
+                    <input id="flight-date" type="date"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class UpdateButton extends React.Component{
+    constructor(){
+        super();
+    }
+
+    updateDiagram() {
+        let departureStation = $("#departure-station-dropdown-button").text();
+        let arrivalStation = $("#arrival-station-dropdown-button").text();
+        let flightDate = $("#flight-date").val();
+        $.ajax({
+            url: "http://localhost:8080/flights?departureStation="+departureStation+"&arrivalStation="+arrivalStation+"&flightDate="+flightDate
+        }).done(function (data) {
+            console.log(data);
+            $("#diagram-data").text(data)
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="btn btn-primary" type="button" onClick={() => this.updateDiagram()}>Update</button>
+                <div className="separator"/>
+                <div id="diagram-data"></div>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    <DropdownFill station="departure"/>, document.getElementById('departure-station-dropdown')
-);
-
-ReactDOM.render(
-    <DropdownFill station="arrival"/>, document.getElementById('arrival-station-dropdown')
+    <PriceDiagram/>
+    ,document.getElementById('price-diagram')
 );
